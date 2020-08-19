c640368b167b5bb61f397ac7ceba026e16cd556d
Deep Agrawal <deepsanjay.agrawal2018@vitstudent.ac.in>
2020-08-19 18:55:18 +0530

    initiated frontend

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+import Navbar from './components/layout/Navbar';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <span>Socialss</span>
+      <div className='nav-links'>
+        <a href='#'>Link</a>
+        <a href='#'>Link</a>
+        <a href='#'>Link</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
